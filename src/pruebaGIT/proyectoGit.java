7246b0d20f734cd898db06a477a7eaa3f36fbb4c
AlejandroGit25 <167419940+AlejandroGit25@users.noreply.github.com>
2024-05-02 11:58:39 +0200

    Update proyectoGit.java

--- src/pruebaGIT/proyectoGit.java
+++ src/pruebaGIT/proyectoGit.java
@@ -6,14 +6,13 @@ public class proyectoGit {
 	public static void main(String[] args) {
 		Integer numero = null;
 		Scanner teclado = new Scanner(System.in);
-		numero=teclado.nextInt();
-		HolaMundo(numero+1);
+	
 		
 		
 	}
 	
 	public static void HolaMundo(Integer numero) {
-		System.out.println("Hola Mundo "+ numero+2);
+
 		
 	}
 }
